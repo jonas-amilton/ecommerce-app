113ad789f4e485f6eb35c8f70e1078aab98abf04
jonas-amilton <jonas96@edu.unisinos.br>
2025-08-08 22:03:51 -0300

    feat: created context for cart features

---
+++ app/context/CartContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+interface CartContextType {
+  cartItems: Product[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (id: number) => void;
+}
+
+export const CartContext = createContext<CartContextType | undefined>(
+  undefined
+);
+
+export const cartProvider = ({ children }: { children: ReactNode }) => {
+  const [cartItems, setCartItems] = useState<Product[]>([]);
+
+  const addToCart = (product: Product) => {
+    setCartItems((prev) => [...prev, product]);
+  };
+
+  const removeFromCart = (id: number) => {
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
