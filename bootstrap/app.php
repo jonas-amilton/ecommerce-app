45f3ed2abf1f458d7ac05701d0334541b2d03410
jonas-amilton <jonas96@edu.unisinos.br>
2026-02-14 18:33:39 -0300

    feat: configura carregamento de rotas segmentadas por subdomínio

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,45 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            $domain = config('app.domain');
+
+            // ADMIN
+            Route::domain("admin.{$domain}")
+                ->middleware('web')
+                ->name('admin.')
+                ->group(base_path('routes/admin/auth.php'));
+
+            Route::domain("admin.{$domain}")
+                ->middleware('web')
+                ->name('admin.')
+                ->group(base_path('routes/admin/web.php'));
+
+            // SELLER
+            Route::domain("seller.{$domain}")
+                ->middleware('web')
+                ->name('seller.')
+                ->group(base_path('routes/seller/auth.php'));
+
+            Route::domain("seller.{$domain}")
+                ->middleware('web')
+                ->name('seller.')
+                ->group(base_path('routes/seller/web.php'));
+
+            // CUSTOMER
+            Route::domain("customer.{$domain}")
+                ->middleware('web')
+                ->name('customer.')
+                ->group(base_path('routes/customer/auth.php'));
+
+            Route::domain("customer.{$domain}")
+                ->middleware('web')
+                ->name('customer.')
+                ->group(base_path('routes/customer/web.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
