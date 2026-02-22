27bf84984008683c5bf6fb980d8f55ac92c851b4
jonas-amilton <jonas96@edu.unisinos.br>
2026-02-22 00:56:29 -0300

    feat(auth): tornar modelos de guard autenticáveis

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
